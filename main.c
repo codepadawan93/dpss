cff9ada74f3d09cce266fec434fd89ea8aa638f6
Erik <kovaroberik@gmail.com>
2020-04-26 16:28:21 +0300

    fixed runtime errors

--- main.c
+++ main.c
@@ -8,8 +8,8 @@
 #include <time.h>
 #include <omp.h>
 #if __has_include(<mpi.h>)
-    #define _IS_MPI 1
     #include <mpi.h>
+	#define _IS_MPI 1
 #endif
 
 #define ECB 1
@@ -40,8 +40,8 @@ int parseArgs(int argc, char* argv[], char ** fileName, char ** key, char ** out
 		} else if (strcmp(argv[i], "-verbose") == 0) {
 			*verbose = 1;
 		} else if (strcmp(argv[i], "-openmpi") == 0){
-                        *openmpi = 1;
-                }
+            *openmpi = 1;
+        }
 	}
 	if (*fileName != NULL && *key != NULL) {
 		return 0;
@@ -125,7 +125,9 @@ int main(int argc, char * argv[]) {
 	char * charKey = NULL;
 	char * outFileName = NULL;
 	int verbose = 0;
-        int openmpi = 0;
+    int openmpi = 0;
+
+	int wSize, rank = 0;
 
 	int success = 
             parseArgs(argc, argv, &fileName, &charKey, &outFileName, &verbose, &openmpi);
@@ -142,25 +144,25 @@ int main(int argc, char * argv[]) {
 
 	    clock_t start, end, delta;
 
-            if (verbose == 1) {
+        if (verbose == 1) {
 			printf("Size=%d", size); printf(CRLF);
 			printf("Input"); printf(CRLF);
 			printArray(input, size, "%c");
-            }
+        }
 
 	    uint8_t* key = makeKey(charKey);
 
 	    if (verbose == 1) {
-		printf("Key"); printf(CRLF);
-		printArray(key, 16, "%c");
-            }
+			printf("Key"); printf(CRLF);
+			printArray(key, 16, "%c");
+        }
 
 	    uint8_t* output = (uint8_t*)malloc(size  * sizeof(uint8_t));
 	    uint8_t* output2 = (uint8_t*)malloc(size * sizeof(uint8_t));
 	    zeroArray(output, size);
 	    zeroArray(output2, size);
 
-            if(openmpi == 0){
+        if(openmpi == 0){
 		    // Sequential
 		    start = clock();
 		    AES_ECB_encrypt(input, key, output, size);
@@ -174,62 +176,64 @@ int main(int argc, char * argv[]) {
 		    // Parallel
 		    int chunkSize = size / CORES;
 		    if (chunkSize < 16) {
-			chunkSize = 16;
+				chunkSize = 16;
 		    }
 		    start = clock();
 		    #pragma omp parallel
 		    #pragma omp for
 		    for (int i = 0; i < CORES; i++){
-			if (i * chunkSize <= size) {
-			    AES_ECB_encrypt(input + (i * chunkSize), key, output + (i * chunkSize), chunkSize);
-			}
+				if (i * chunkSize <= size) {
+			    	AES_ECB_encrypt(input + (i * chunkSize), key, output + (i * chunkSize), chunkSize);
+				}
 		    }
 		    end = clock();
 		    delta = end - start;
 		    printf("Parallel took %ld ms.", delta); printf(CRLF);
 		}
 		if(openmpi == 1){
-                    // Open MPI
+            // Open MPI
 		    #ifndef _IS_MPI
-                    printf("OpenMpi not supported."); printf(CRLF);
-                    #else
-                    MPI_Init(NULL, NULL);
-                    int wSize, rank;
-                    MPI_Comm_size(MPI_COMM_WORLD, &wSize);
-                    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-                    int elementsPerProc = size / wSize;
-                    uint8_t * sub = (uint8_t *)malloc(sizeof(uint8_t) * elementsPerProc);
-                    MPI_Scatter(input, elementsPerProc, MPI_INT, sub, elementsPerProc, MPI_INT, 0, MPI_COMM_WORLD);
-                    start = clock();
-		    AES_ECB_encrypt(input + (rank * elementsPerProc), key, sub, elementsPerProc);
-                    MPI_Gather(&sub, elementsPerProc, MPI_INT, output + (rank * elementsPerProc), elementsPerProc, MPI_INT, 0, MPI_COMM_WORLD);
-                    end = clock();
-		    delta = end - start;
-		    printf("OpenMpi took %ld ms.", delta); printf(CRLF);
-                    MPI_Finalize();
-                    #endif
-               }
+        		printf("OpenMpi not supported."); printf(CRLF);
+            #else
+                MPI_Init(NULL, NULL);
+                MPI_Comm_size(MPI_COMM_WORLD, &wSize);
+                MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+                int elementsPerProc = size / wSize;
+                uint8_t * sub = (uint8_t *)malloc(elementsPerProc * sizeof(uint8_t));
+                start = clock();
+				MPI_Scatter(input, elementsPerProc, MPI_CHAR, 
+							sub, elementsPerProc, MPI_CHAR, 0, MPI_COMM_WORLD);
+		    	AES_ECB_encrypt(input + (rank * elementsPerProc), key, sub, elementsPerProc);
+                MPI_Gather(&sub, elementsPerProc, MPI_CHAR, 
+							output + (rank * elementsPerProc), elementsPerProc, MPI_CHAR, 0, MPI_COMM_WORLD);
+                end = clock();
+				delta = end - start;
+		    	printf("OpenMpi took %ld ms.", delta); printf(CRLF);
+                MPI_Finalize();
+            #endif
+        }
 
 		// Logging and verification
-		if (verbose == 1) {
-			printf("Output"); printf(CRLF);
-			printArray(output, size, "%.2X ");
-		}
-		start = clock();
-		if (outFileName != NULL) {
-			writeFile(outFileName, output, size, "%.2X");
-		}
-		end = clock();
-		delta = end - start;
-		printf("Writing took %ld ms.", delta); printf(CRLF);
+		if(rank == 0){
+			if (verbose == 1) {
+				printf("Output"); printf(CRLF);
+				printArray(output, size, "%.2X ");
+			}
+			start = clock();
+			if (outFileName != NULL) {
+				writeFile(outFileName, output, size, "%.2X");
+			}
+			end = clock();
+			delta = end - start;
+			printf("Writing took %ld ms.", delta); printf(CRLF);
 
-		AES_ECB_decrypt(output, key, output2, size);
+			AES_ECB_decrypt(output, key, output2, size);
 
-		if (verbose == 1) {
-			printf("Output 2"); printf(CRLF);
-			printArray(output2, size, "%c");
+			if (verbose == 1) {
+				printf("Output 2"); printf(CRLF);
+				printArray(output2, size, "%c");
+			}
 		}
-		return 0;
 	}
+	return 0;
 }
-
--- test.sh
+++ test.sh
@@ -4,5 +4,5 @@ echo "Running test 1 - linear + parallel";
 echo "Running test 2 - linear + parallel verbose" || echo "Test 1 failed";
 ./dpss -file ./resources/testfile.txt -key testpassword1234 -out ./output/testfile.enc -verbose &&
 echo "Running test 3 - OpenMPI" || echo "Test 2 failed";
-mpirun -np 2 ./mpidpss -file ./resources/bigfile.txt -key testpassword1234 -out ./output/bigfile_mpi.enc -openmpi &&
+mpirun -np 2 ./mpidpss -file ./resources/testfile.txt -key testpassword1234 -out ./output/testfile_mpi.enc -openmpi -verbose &&
 echo "Tests ran successfully" || echo "Test 3 failed"
